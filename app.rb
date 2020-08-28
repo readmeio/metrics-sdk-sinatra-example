c680d8f84017883d15dd0c0f7b9557c4d0d84b51
Joël Quenneville <joelq@thoughtbot.com>
2020-08-28 13:13:33 -0400

    Authenticate with Rack::Auth::Basic

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require "sinatra"
 require "sinatra/json"
+require "rack/auth/basic"
+require "dotenv/load"
+
+use Rack::Auth::Basic do |username, password|
+  username == ENV.fetch("USERNAME") && password == ENV.fetch("PASSWORD")
+end
 
 get "/hello" do
   "hello"

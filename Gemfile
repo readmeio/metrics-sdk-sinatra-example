01c0189f01823217091952fa64f4d13414596420
Joël Quenneville <joelq@thoughtbot.com>
2020-08-28 14:47:40 -0400

    Configure Readme Metrics

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source "https://rubygems.org"
 
 gem "dotenv"
 gem "rack"
+gem "readme-metrics"
 gem "sinatra"
 gem "sinatra-contrib"
--- app.rb
+++ app.rb
@@ -1,10 +1,18 @@
+require "dotenv/load"
+require "rack/auth/basic"
+require "readme/metrics"
 require "sinatra"
 require "sinatra/json"
-require "rack/auth/basic"
-require "dotenv/load"
 
 use Rack::Auth::Basic do |username, password|
   username == ENV.fetch("USERNAME") && password == ENV.fetch("PASSWORD")
+end
+
+use Readme::Metrics, buffer_length: 1, reject_params: ["Authorization"], api_key: ENV.fetch("README_API_KEY") do
+  { id: ENV.fetch("USERNAME"),
+    label: ENV.fetch("USERNAME"),
+    email: "user@example.com"
+  }
 end
 
 get "/hello" do
